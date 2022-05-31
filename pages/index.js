448432635d946995a207f6bc446ce451700e1378
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-05-31 15:24:07 +0400

    SSG with dynamic parameters

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
-const Home = () =>{
-  return <h1>Next JS pre-rendering</h1>
-}
+import Link from "next/link";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Next JS pre-rendering</h1>
+      <Link href="/users">
+        <a>Users</a>
+      </Link>
+      <Link href="/posts">
+        <a>Posts</a>
+      </Link>
+    </>
+  );
+};
 
 export default Home;
